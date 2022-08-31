a8720bf9006d55f9062633c629c5688701023a73
j.kodsi <j.kodsi@smartsoft-me.com>
2022-08-31 13:51:20 +0300

    Add BaseOperator to Multifilter, default is "and"

--- EFSorter/Filters/MultiFilter.cs
+++ EFSorter/Filters/MultiFilter.cs
@@ -1,11 +1,11 @@
 ﻿namespace EFSorter.Filters
 {
-    public record MultiFilter(List<Filter> Filters)
+    public record MultiFilter(List<Filter> Filters, string BaseOperator = "and")
     {
         public override string ToString()
         {
             if (Filters.Count > 0)
-                return string.Join(" and ", Filters.Select(f => f.ToString()));
+                return string.Join(" " + BaseOperator + " ", Filters.Select(f => f.ToString()));
             return string.Empty;
         }
     }
