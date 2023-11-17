f9f191988fc8ca5d12ff2968d6ffc871196f8bf0
m.jawad@smartsoft-me.com <m.jawad@smartsoft-me.com>
2023-11-17 10:52:42 +0200

    string concatination

--- EFSorter/Filters/Condition.cs
+++ EFSorter/Filters/Condition.cs
@@ -6,6 +6,7 @@
         public string Type { get; set; }
         public string Operator { get; set; }
         public string Value { get; set; }
+
         public Condition() { }
 
         public Condition(string field, string type, string @operator, string value)
@@ -13,73 +14,76 @@
             Field = field;
             Type = type;
             Operator = @operator;
-            Value = value;
+            Value = type == "number" ? value : "\"" + value + "\"";
         }
 
         public string Build() => Type.ToLowerInvariant() switch
         {
-            "text" => mapTextCondition(),
-            "number" => mapNumberCondition(),
-            "date" => mapDateCondition(),
-            "guids" => mapGuidsCondition(),
-            "object" => mapObjectCondition(),
+            "text" => MapTextCondition(),
+            "number" => MapNumberCondition(),
+            "date" => MapDateCondition(),
+            "guids" => MapGuidsCondition(),
+            "object" => MapObjectCondition(),
             _ => "",
         };
 
-        private string mapDateCondition()
+        private string MapDateCondition()
         {
             var isDate = DateTime.TryParse(Value, out var validDate);
-            if (isDate)
-                return Operator switch
-                {
-                    "equals" => Field + "= \"" + validDate.ToString("s") + "\"",
-                    "notEqual" => Field + "!= \"" + validDate.ToString("s") + "\"",
-                    "lessThan" => Field + "< \"" + validDate.ToString("s") + "\"",
-                    "greaterThan" => Field + "> \"" + validDate.ToString("s") + "\"",
-                    "blank" => Field + "==null",
-                    "notBlank" => Field + "!=null",
-                    _ => "",
-                };
-            return "";
+            if (!isDate) return "";
+
+            return Operator switch
+            {
+                "equals" => $"{Field} = {validDate:s}",
+                "notEqual" => $"{Field} != {validDate:s}",
+                "lessThan" => $"{Field} < {validDate:s}",
+                "greaterThan" => $"{Field} > {validDate:s}",
+                "blank" => $"{Field} == null",
+                "notBlank" => $"{Field} != null",
+                _ => "",
+            };
         }
 
-        private string mapNumberCondition() => Operator switch
+        private string MapNumberCondition() => Operator switch
         {
-            "equals" => Field + "=" + Value,
-            "notEqual" => Field + "!=" + Value,
-            "lessThan" => Field + "<" + Value,
-            "lessThanOrEqual" => Field + "<=" + Value,
-            "greaterThan" => Field + ">" + Value,
-            "greaterThanOrEqual" => Field + ">=" + Value,
-            "blank" => Field + "==null",
-            "notBlank" => Field + "!=null",
+            "equals" => $"{Field} = {Value}",
+            "notEqual" => $"{Field} != {Value}",
+            "lessThan" => $"{Field} < {Value}",
+            "lessThanOrEqual" => $"{Field} <= {Value}",
+            "greaterThan" => $"{Field} > {Value}",
+            "greaterThanOrEqual" => $"{Field} >= {Value}",
+            "blank" => $"{Field} == null",
+            "notBlank" => $"{Field} != null",
             _ => "",
         };
-        private string mapTextCondition() => Operator switch
+
+        private string MapTextCondition() => Operator switch
         {
-            "equals" => Field + "=\"" + Value + "\"",
-            "notEqual" => Field + "!=\"" + Value + "\"",
-            "contains" => Field + ".Contains(\"" + Value + "\")",
-            "contains_i" => Field + ".ToLower().Contains(\"" + Value.ToLower() + "\")",
-            "notContains" => "!" + Field + ".Contains(\"" + Value + "\")",
-            "notContains_i" => "!" + Field + ".ToLower().Contains(\"" + Value.ToLower() + "\")",
-            "startsWith" => Field + ".StartsWith(\"" + Value + "\")",
-            "endsWith" => Field + ".EndsWith(\"" + Value + "\")",
-            "blank" => "string.IsNullOrEmpty(" + Field + ")",
-            "notBlank" => "!string.IsNullOrEmpty(" + Field + ")",
+            "equals" => $"{Field} = {Value}",
+            "notEqual" => $"{Field} != {Value}",
+            "contains" => $"{Field}.Contains({Value})",
+            "contains_i" => $"{Field}.ToLower().Contains({Value.ToLower()})",
+            "notContains" => $"!{Field}.Contains({Value})",
+            "notContains_i" => $"!{Field}.ToLower().Contains({Value.ToLower()})",
+            "startsWith" => $"{Field}.StartsWith({Value})",
+            "endsWith" => $"{Field}.EndsWith({Value})",
+            "blank" => $"string.IsNullOrEmpty({Field})",
+            "notBlank" => $"!string.IsNullOrEmpty({Field})",
             _ => "",
         };
-        private string mapGuidsCondition() => Operator switch
+
+        private string MapGuidsCondition() => Operator switch
         {
-            "in" => Field + ".Any(x => x.Guid ==\"" + Value + "\")",
-            "notIn" => "!" + Field + ".Any(x => x.Guid == \"" + Value + "\")",
-            _ => ""
+            "in" => $"{Field}.Any(x => x.Guid == {Value})",
+            "notIn" => $"!{Field}.Any(x => x.Guid == {Value})",
+            _ => "",
         };
-        private string mapObjectCondition() => Operator switch
+
+        private string MapObjectCondition() => Operator switch
         {
-            "blank" => "" + Field + "==null",
-            "notBlank" => "" + Field + "!=null",
-            _ => ""
+            "blank" => $"{Field} == null",
+            "notBlank" => $"{Field} != null",
+            _ => "",
         };
     }
 }
