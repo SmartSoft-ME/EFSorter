a9912c71c5b7c196aa63ee285498cd61b3976d5b
j.kodsi <j.kodsi@smartsoft-me.com>
2023-11-17 15:40:56 +0200

    clean up main

--- EFSorter.ConsoleApp/Program.cs
+++ EFSorter.ConsoleApp/Program.cs
@@ -9,36 +9,35 @@ using EFSorter.Filters;
 using OfficeOpenXml;
 
 ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-
 Randomizer.Seed = new Random(69);
 var Ids = 0;
-
 var test = new Faker<Person>()
     .RuleFor(p => p.Id, f => Ids++)
     .RuleFor(p => p.FirstName, f => f.Name.FullName())
     .RuleFor(p => p.BirthDay, f => f.Date.Past(50).Date)
     .RuleFor(p => p.Addresses, f => new() { new(Guid.NewGuid(), "TEST"), new(Guid.NewGuid(), "TEST1") })
-    .RuleFor(p => p.Natchos, f => new() { new(Guid.NewGuid(), "Natcho"), new(Guid.NewGuid(), "Natcho") });
+    .RuleFor(p => p.Nachos, f => new() { new(Guid.NewGuid(), "Natcho"), new(Guid.NewGuid(), "Natcho") });
+
+var people = test.Generate(20);
+people.First().Addresses.FirstOrDefault()!.Guid = new Guid("e97eef8f-5636-42a5-894e-9401c65c21dc");
 Console.WriteLine("Before Applying the filter");
 Console.WriteLine("<------------------------>");
-var list = test.Generate(20);
-list.FirstOrDefault()!.Addresses.FirstOrDefault()!.Guid = new Guid("e97eef8f-5636-42a5-894e-9401c65c21dc");
-foreach (var item in list)
+foreach (var person in people)
 {
-    if (item.Id % 2 == 1)
-        item.Blank = "A";
-    else
-        item.Blank = "";
+    person.Blank = person.Id % 2 == 1 ? "A" : "";
+    Console.WriteLine(person.FirstName + ", " + person.BirthDay);
 }
-var filters = new List<Filter>
+
+// filters
+var dateFilter = new List<Filter>
 {
     new(new("BirthDay","date","equals","12/17/1996"),null,null)
 };
-var anotherFilter = new List<Filter>
+var blankFilter = new List<Filter>
 {
     new(new("Blank","text","notBlank",""),null,null)
 };
-var yetAnotherFilter = new List<Filter>
+var notInFilter = new List<Filter>
 {
     new(new("Addresses","guids","notIn","e97eef8f-5636-42a5-894e-9401c65c21dc"),null,null)
 };
@@ -48,30 +47,19 @@ var threeFilters = new List<Filter>
     new(new("Id", "number", "equals", "1"), null, null)
 };
 
-MultiFilter filter = new(filters);
-MultiFilter filter1 = new(anotherFilter);
-MultiFilter filter2 = new(yetAnotherFilter);
-MultiFilter filter3 = new(threeFilters, "and");
-foreach (Person p in list)
-{
-    Console.WriteLine(p.BirthDay);
-}
-var res = list.AsQueryable().ApplyFilters(filter, null);
-var res1 = list.AsQueryable().ApplyFilters(filter1, null);
-var res2 = list.AsQueryable().ApplyFilters(filter2, null);
-var res3 = list.AsQueryable().ApplyFilters(null, null);
-var res4 = list.AsQueryable().ApplyFilters(filter3, null);
+var filter = new MultiFilter(dateFilter);
+var filter1 = new MultiFilter(blankFilter);
+var filter2 = new MultiFilter(notInFilter);
+var filter3 = new MultiFilter(threeFilters, "and");
+
+var res = people.AsQueryable().ApplyFilters(filter3, null);
+
 Console.WriteLine("After Applying the filter");
 Console.WriteLine("<------------------------>");
-foreach (Person p in res4.ToList())
-{
-    Console.WriteLine(p);
-}
+res.ToList().ForEach(Console.WriteLine);
 
-var specificDate = list.FirstOrDefault(p => p.BirthDay == DateTime.Parse("12/17/1996 12:00:00 AM"));
-Console.WriteLine(specificDate);
-var t = new ExcelGenerator<Person>(list);
-//record Person(int Id, string FirstName, DateTime BirthDay);
+var t = new ExcelGenerator<Person>(people);
+
 public record Person
 {
     public int Id { get; set; }
@@ -79,34 +67,28 @@ public record Person
     public DateTime BirthDay { get; set; }
     public string? Blank { get; set; }
     public List<Address> Addresses { get; set; } = new();
-    public List<Natcho> Natchos { get; set; } = new();
+    public List<Nacho> Nachos { get; set; } = new();
 }
 
 public record Address
 {
+    public Guid Guid { get; set; }
+    public string Name { get; set; }
     public Address(Guid guid, string name)
     {
         Guid = guid;
         Name = name;
     }
-    public override string ToString()
-    {
-        return Name;
-    }
+    public override string ToString() => Name;
+}
+public record Nacho
+{
     public Guid Guid { get; set; }
     public string Name { get; set; }
-}
-public record Natcho
-{
-    public Natcho(Guid guid, string name)
+    public Nacho(Guid guid, string name)
     {
         Guid = guid;
         Name = name;
     }
-    public override string ToString()
-    {
-        return Name;
-    }
-    public Guid Guid { get; set; }
-    public string Name { get; set; }
+    public override string ToString() => Name;
 }
